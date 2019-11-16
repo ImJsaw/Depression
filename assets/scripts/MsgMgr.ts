fda9263d1ea2c1b5c6909d846941bcd004947673
flyinglimao <hi@limaois.me>
2019-11-16 22:38:59 +0800

    finish msg box

--- assets/scripts/MsgMgr.ts
+++ assets/scripts/MsgMgr.ts
@@ -12,13 +12,18 @@ const {ccclass, property} = cc._decorator;
 
 @ccclass
 export default class MsgMgr extends cc.Component {
+    private static selectionChar = ['A', 'B', 'C', 'D']
+    private nextButton: cc.Node
     private nameText: cc.Node
     private contentText: cc.Node
     private buttonGroup: cc.Node
     private buttons: Array<cc.Node> = []
-    private static selectionChar = ['A', 'B', 'C', 'D']
+    private scripts: Object = {}
+    private playing: string
+    private playingProcess: number
 
-    start () {
+    async start () {
+        this.nextButton = cc.find('MsgBox/Next', this.node)
         this.nameText = cc.find('MsgBox/Name/Background/Text', this.node)
         this.contentText = cc.find('MsgBox/Content/Background/Text', this.node)
         this.buttonGroup = cc.find('MsgBox/Buttons', this.node)
@@ -26,31 +31,86 @@ export default class MsgMgr extends cc.Component {
         this.buttons.push(cc.find('ButtonB', this.buttonGroup))
         this.buttons.push(cc.find('ButtonC', this.buttonGroup))
         this.buttons.push(cc.find('ButtonD', this.buttonGroup))
+
+        this.node.on('click', this.next)
+        await this.load('腳本/example.json')
+
+        this.node.active = false
+        
+        window['some'] = () => this.play('scriptname')
     }
 
-    normal (name: string, content: string) {
+    load (file: string) {
+        return new Promise((res, rej) => {
+            cc.loader.loadRes(file, (err, obj) => {
+                if (err) console.error(err)
+                if (this.scripts) {
+                    Object.keys(obj.json).forEach(e => {
+                        this.scripts[e] = obj.json[e]
+                    })
+                } else {
+                    this.scripts = obj.json
+                }
+                res(this.scripts)
+            })
+        })
+    }
+
+    play (script: string, init: number = 0) {
+        this.node.active = true
+        this.playing = script
+        this.playingProcess = init
+        if (this.scripts[this.playing][this.playingProcess].selections) {
+            // @ts-ignore
+            this.select(this.scripts[this.playing][this.playingProcess])
+        } else {
+            // @ts-ignore
+            this.normal(this.scripts[this.playing][this.playingProcess])
+        }
+    }
+
+    next () {
+        if (this.scripts[this.playing][this.playingProcess].selections) return false
+        
+        this.playingProcess += 1 
+        if (this.scripts[this.playing][this.playingProcess].selections) {
+            // @ts-ignore
+            this.select(this.scripts[this.playing][this.playingProcess])
+        } else if (this.scripts[this.playing].length <= this.playingProcess) {
+            this.node.active = false
+            return false
+        } else {
+            // @ts-ignore
+            this.normal(this.scripts[this.playing][this.playingProcess])
+        }
+        return true
+    }
+
+    normal (obj: {name: string, content: string}) {
+        this.nextButton.active = true
         this.buttonGroup.active = false
-        this.nameText.getComponent(cc.Label).string = name
-        this.contentText.getComponent(cc.Label).string = content
+        this.nameText.getComponent(cc.Label).string = obj.name
+        this.contentText.getComponent(cc.Label).string = obj.content
     }
 
-    select (name: string, content: string, selections: Array<{label: string, action: string, data?: any}>) {
+    select (obj: {name: string, content: string, selections: Array<{content: string, event: string, data?: any}>}) {
+        this.nextButton.active = false
         this.buttonGroup.active = true
         this.buttons.forEach(n => {
             n.active = false
             n.getComponent(cc.Button).clickEvents = []
         })
-        this.nameText.getComponent(cc.Label).string = name
-        this.contentText.getComponent(cc.Label).string = content
-        for (let i in selections) {
+        this.nameText.getComponent(cc.Label).string = obj.name
+        this.contentText.getComponent(cc.Label).string = obj.content
+        for (let i in obj.selections) {
             this.buttons[i].active = true
-            this.buttons[i].getComponentInChildren(cc.Label).string = MsgMgr.selectionChar[i] + '. ' + selections[i].label
+            this.buttons[i].getComponentInChildren(cc.Label).string = MsgMgr.selectionChar[i] + '. ' + obj.selections[i].content
 
             let tmpEventHandler = new cc.Component.EventHandler()
             tmpEventHandler.target = this.node
             tmpEventHandler.component = "MsgEvents"
-            tmpEventHandler.handler = selections[i].action
-            Object.assign(tmpEventHandler, selections[i].data)
+            tmpEventHandler.handler = obj.selections[i].event
+            Object.assign(tmpEventHandler, obj.selections[i].data)
 
             this.buttons[i].getComponent(cc.Button).clickEvents.push(tmpEventHandler)
         }
