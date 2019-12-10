828293dd8436aef92425d219dc8f18bbe2cdc38c
賓 <bing5300@gmail.com>
2019-12-11 00:32:48 +0800

    start & loading scene complete

--- assets/scripts/SceneLoading.ts
+++ assets/scripts/SceneLoading.ts
@@ -9,23 +9,12 @@ const { ccclass, property } = cc._decorator;
 @ccclass
 export default class SceneLoading extends cc.Component {
 
-    @property(cc.Label) tipLabel: cc.Label = null;
+    @property(cc.VideoPlayer) openingVideo : cc.VideoPlayer = null;
+    @property(cc.Node) bg : cc.Node = null;
 
-    @property(cc.Node) loadingBar: cc.Node = null;
-    @property(Number) minLoadingBarWidth: number = 20;
-    @property(Number) maxLoadingBarWidth: number = 1170;
-
-    @property(cc.Node) loadingBarHead: cc.Node = null;
-    @property(cc.Node) loadingBarTrail: cc.Node = null;
-    @property(Number) showLoadingBarTrailPos: number = -545;
-    @property(Number) minLoadingBarHeadPos: number = -580;
-    @property(Number) maxLoadingBarHeadPos: number = 570;
-
-    @property(cc.Node) button: cc.Node = null;
-    stateStr: string;
-    progress: number;
-    isLoading: boolean;
-
+    progress : number = 0;
+    isLoadingComplete: boolean;
+    isVideoComplete: boolean;
 
     onLoad() {
         if (!Game.Inst.init) {
@@ -33,26 +22,37 @@ export default class SceneLoading extends cc.Component {
             return;
         }
         Game.Inst.utils.resize();
-        this.progress = 0;
-        //this.tipLabel.string = this.stateStr;
         this.startPreloading();
+        this.registerVideo();
     }
 
-    start() {
-        // Game.Inst.animationMgr.play("LogoAnim", 1.0, true);
+    registerVideo(){
+        this.openingVideo.node.active = true;
+        this.isVideoComplete = false;
+        this.openingVideo.node.on("ready-to-play",this.startVideo, this);
+        this.openingVideo.node.on("completed",this.onVideoComplete,this);
+    }
+
+    startVideo(){
+        this.openingVideo.play();
+        this.isVideoComplete = true;
+    }
+
+    onVideoComplete(){
+        this.openingVideo.node.active = false;
+        this.bg.active = false;
+        //check goto game
+        this.isVideoComplete = true;
+        if(this.isLoadingComplete){
+            Game.Inst.mainStateMgr.changeState(GameState.Game);
+        }
     }
 
     startPreloading() {
-        this.tipLabel.string = "0 / 100";
-        this.loadingBar.setContentSize(this.minLoadingBarWidth,this.loadingBar.getContentSize().height);
-        this.loadingBarHead.position = cc.v2(this.minLoadingBarHeadPos);
-        this.loadingBarTrail.opacity = 0;
-
-        this.isLoading = true;
+        this.isLoadingComplete = false;
 
         Game.Inst.resourcesMgr.preload(ResourceIndex.Game, (p) => {
             this.progress = p;
-
             if (this.progress >= 1.0) {
                 this.onLoadComplete();
             }
@@ -60,35 +60,15 @@ export default class SceneLoading extends cc.Component {
     }
 
     onLoadComplete() {
-        if (!this.isLoading)
-            return;
-
-        this.isLoading = false;
-        //this.stateStr = Game.Inst.text.get("SwitchingScene");
-        
-        if(this.button == null)
-            this.button =this.node.getChildByName("Game");
-        //this.button.opacity = 255;
-        Game.Inst.mainStateMgr.changeState(GameState.Game);
+        this.isLoadingComplete = true;
+        //check goto game
+        if(this.isVideoComplete){
+            Game.Inst.mainStateMgr.changeState(GameState.Game);
+        }
     }
     
     enterGame(){
         Game.Inst.mainStateMgr.changeState(GameState.Game);
     }
-    // called every frame, uncomment this function to activate update callback
-    update(dt) {
-        if (this.isLoading) {
-            this.tipLabel.string = Math.floor(this.progress * 100).toString() + " / 100";
-            this.loadingBar.setContentSize(cc.misc.lerp(this.minLoadingBarWidth,this.maxLoadingBarWidth,this.progress),this.loadingBar.getContentSize().height);
-            this.loadingBarHead.position = cc.v2(cc.misc.lerp(this.minLoadingBarHeadPos,this.maxLoadingBarHeadPos,this.progress));
-            this.loadingBarTrail.opacity = cc.misc.lerp(0,255,MiscHelper.inverseLerp(this.minLoadingBarHeadPos,this.showLoadingBarTrailPos,this.loadingBarHead.position.x));
-        }
-        else {
-            this.tipLabel.string = "100 / 100";
-            this.loadingBar.setContentSize(this.maxLoadingBarWidth,this.loadingBar.getContentSize().height);
-            this.loadingBarHead.position = cc.v2(this.maxLoadingBarHeadPos);
-            this.loadingBarTrail.opacity = 255;
-        }
-
-    }
+    
 }
--- assets/scripts/SceneStart.ts
+++ assets/scripts/SceneStart.ts
@@ -6,11 +6,38 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class SceneStart extends cc.Component {
 
+    @property(cc.Node)
+    start1 : cc.Node = null;
+
+    @property(cc.Node)
+    start2 : cc.Node = null;
+
+    @property(cc.Node)
+    startButton : cc.Node = null;
+
     onLoad(){
         cc.view.enableAntiAlias(true);
 
         Game.Inst.init = true;
         Game.Inst.utils.resize();
+
+        this.regEvent();
+    }
+
+    regEvent(){
+        let self = this;
+        this.startButton.on(cc.Node.EventType.MOUSE_ENTER, function (event) {
+            self.start2.active = true;
+        });
+
+        this.startButton.on(cc.Node.EventType.MOUSE_LEAVE, function (event) {
+            self.start2.active = false;
+        });
+
+        this.startButton.on(cc.Node.EventType.TOUCH_START, function (event) {
+            self.enterGame();
+        });
+
     }
 
     enterGame(){
