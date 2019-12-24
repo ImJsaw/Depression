868b6bdd304e80b394c67ebf4fc0ad305fb1b3cb
aiiuii <qaz620300103@gmail.com>
2019-12-25 03:49:22 +0800

    scenario complete

--- assets/scripts/MsgEvents.ts
+++ assets/scripts/MsgEvents.ts
@@ -1,36 +1,47 @@
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 import * as Define from "./Define";
 import UIMgr from "./UIMgr";
 
 @ccclass
 export default class MsgEvents extends cc.Component {
 
-    @property(cc.Node) demo:cc.Node=null;
+    @property(cc.Node) demo: cc.Node = null;
 
     example() {
         window['stopScript']()
         cc.log("adfs")
     }
+    stop() {
+        window['stopScript']()
 
-    help(){
-        this.demo.active=false
+    }
+    help() {
+        this.demo.active = false
     }
 
-    ingore(){
-        this.demo.active=true
+    ingore() {
+        this.demo.active = true
     }
-
-    goToSchool(){
+    strongAndFatEnd() {
+        let backetballCourt = cc.find("Canvas/centerAnchor/reality/bridge/backetballCourt");
+        backetballCourt.getChildByName("strongAndFat").active = false;
+    }
+    goToSchool() {
         window['stopScript']()
         Define.GameInfo.Inst.curRealityScene = Define.RealityScene.school;
         UIMgr.Inst.realityMgr.changeScene(Define.GameInfo.Inst.curRealityScene, true);
 
     }
 
-    goToHome(){
+    goToHome() {
         window['stopScript']()
         Define.GameInfo.Inst.curRealityScene = Define.RealityScene.home;
         UIMgr.Inst.realityMgr.changeScene(Define.GameInfo.Inst.curRealityScene, true);
         cc.find('Canvas/Msg').getComponent('MsgMgr').play("backToHome");
+    }
+
+    princessAndBad() {
+        window['stopScript']()
+        cc.find('Canvas/Msg').getComponent('MsgMgr').play("princessAndBad");
     }
 }
